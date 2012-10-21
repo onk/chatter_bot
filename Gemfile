7220cc314681f94d2000dd77b1f48058e9f36751
onk <takafumi.onaka@gmail.com>
2012-10-22 02:11:12 +0900

    add activerecord gem

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source :rubygems
 gem "pit"
 gem "databasedotcom"
 
+gem "activerecord", require: "active_record"
+gem "mysql2"
+
--- lib/chatter_bot.rb
+++ lib/chatter_bot.rb
@@ -1,24 +1,29 @@
 module ChatterBot
-  def self.main
+  def self.setup
     config = {
       client_id:     Pit.get("chatter_bot")["client_id"],
       client_secret: Pit.get("chatter_bot")["client_secret"],
       version:       "23.0"
     }
-    client = Databasedotcom::Client.new(config)
-
+    @client = Databasedotcom::Client.new(config)
     h = {
       token:         Pit.get("chatter_bot")["access_token"],
       instance_url:  "https://ap.salesforce.com"
     }
-    client.authenticate(h)
+    @client.authenticate(h)
 
-    feed_items = Databasedotcom::Chatter::CompanyFeed.find(client)
+    db_config = YAML.load_file("config/database.yml")
+    ActiveRecord::Base.establish_connection(db_config)
+  end
+
+  def self.main
+    feed_items = Databasedotcom::Chatter::CompanyFeed.find(@client)
     p "========================================================================="
     p feed_items.inspect
     p "========================================================================="
   end
 end
 
+ChatterBot.setup
 ChatterBot.main
 
